7ce2aebb6a1be293f8c5623346a8e3a05b9031a5
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-08-08 20:09:00 -0300

    feat: add teacher list as component

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars2.githubusercontent.com/u/15057026?s=460&u=0f786accd291f82ae7c9b86797a66d26f4f98f9a&v=4"
+          alt="Profile"
+        />
+        <div>
+          <strong>Adroaldo Pagliari</strong>
+          <span>Física</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta em programação, matéria, movimento e energia.
+      </p>
+
+      <footer>
+        <p>
+          Preço/Hora
+              <strong>R$ 200,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+              Entrar em contato
+            </button>
+      </footer>
+    </article>
+  )
+}
+
+export default TeacherItem;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -11,20 +12,26 @@ function TeacherList() {
         <form id="search-teachers">
           <div className="input-block">
             <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
+            <input type="text" id="subject" />
           </div>
 
           <div className="input-block">
             <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" id="week_day"/>
+            <input type="text" id="week_day" />
           </div>
 
           <div className="input-block">
             <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
+            <input type="text" id="time" />
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
     </div>
   )
 }
