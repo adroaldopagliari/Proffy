789ea8856a7f3f5e0b910766d1bf380011c8f69f
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-08-12 14:37:23 -0300

    refactor: convert routes default function into an arrow one

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -5,14 +5,14 @@ import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
 
-function Routes() {
+const Routes: React.FC = () => {
   return (
     <BrowserRouter>
-      <Route path='/' exact component={Landing}/>
-      <Route path='/study' exact component={TeacherList}/>
-      <Route path='/give-classes' exact component={TeacherForm}/>
+      <Route path="/" exact component={Landing} />
+      <Route path="/study" exact component={TeacherList} />
+      <Route path="/give-classes" exact component={TeacherForm} />
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default Routes;
