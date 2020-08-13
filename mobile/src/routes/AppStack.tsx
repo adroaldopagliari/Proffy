97d67becb8c0e033b4e116bf0cd390317abf56da
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-08-13 14:28:50 -0300

    feat: add teacher list page and udpdate routes file

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from '../pages/Landing';
+import GiveClasses from '../pages/GiveClasses';
+import TeacherList from '../pages/TeacherList';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -11,6 +13,8 @@ const AppStack: React.FC = () => {
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Landing" component={Landing} />
+        <Screen name="GiveClasses" component={GiveClasses} />
+        <Screen name="TeacherList" component={TeacherList} />
       </Navigator>
     </NavigationContainer>
   );
