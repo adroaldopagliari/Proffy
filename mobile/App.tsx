b724cb36bb2697ffa4b4fdbde2c221748bfe20e8
Adroaldo J. Pagliari <adroaldo@outlook.com>
2020-08-13 11:12:05 -0300

    feat: add archive and poppins google fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,37 @@
+/* eslint-disable react/style-prop-object */
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+
+import {
+  Archivo_400Regular,
+  Archivo_700Bold,
+  useFonts,
+} from '@expo-google-fonts/archivo';
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
+
 import Landing from './src/pages/Landing';
 
-export default function App() {
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <>
       <Landing />
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </>
   );
-}
+};
 
-
+export default App;
